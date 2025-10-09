9f439a706fa1c58c90dac3381fd75d98eec42f97
Rosalia Salsabila <152388107+rosalsa@users.noreply.github.com>
2025-10-09 11:55:13 +0700

    Add files via upload

---
+++ Pertemuan 2/pertemuan_2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int luas, panjang, lebar;
+
+    cout << "Masukkan panjang : ";
+    cin >> panjang;
+
+    cout << endl;
+
+    cout << "Masukkan lebar : ";
+    cout << "ini baru";
+    cin >> lebar;
+
+    luas = panjang * lebar;
+
+    cout << "Luas nya = " << luas;
+
+    // int luas, alas, tinggi;
+
+    // alas = 10;
+    // tinggi = 12;
+
+    // luas = (alas * tinggi) / 2;
+
+    // cout << "luasnya adalah : " << luas;
+
+}
